6d27144feacee32c52a3cb707215c9cc8e1a3bfe
sihar7 <muhammadsigit.hd20@gmail.com>
2025-06-15 03:44:48 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use L5Swagger\Http\Controllers\SwaggerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/docs', [SwaggerController::class, 'docs']);
+
