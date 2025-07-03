670e7430bdc878a5479633b673865a7ea48c5819
Sigit Hardianto <69493819+sihar7@users.noreply.github.com>
2025-07-03 20:16:38 +0700

    ehem ehem

--- app/Http/Middleware/CorsOptions.php
+++ app/Http/Middleware/CorsOptions.php
@@ -3,19 +3,43 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CorsOptions
 {
-    public function handle($request, Closure $next)
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
     {
+        $allowedOrigins = [
+            'https://virsign.netlify.app',
+            'http://localhost:5173'
+        ];
+
+        $origin = $request->headers->get('Origin');
+
         if ($request->isMethod('OPTIONS')) {
-            return response('', 204)
-                ->header('Access-Control-Allow-Origin', 'https://virsign.netlify.app')
-                ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-                ->header('Access-Control-Allow-Credentials', 'true');
+            $response = response()->noContent();
+        } else {
+            $response = $next($request);
         }
 
-        return $next($request);
+        if (in_array($origin, $allowedOrigins)) {
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
+        }
+
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->headers->set('Vary', 'Origin');
+        $response->headers->set('Access-Control-Max-Age', '86400');
+
+        return $response;
     }
 }
